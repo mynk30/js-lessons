dbe0717f97e0c1e48036e15b774648233a01a044
Mayank Soni <92715309+mynk30@users.noreply.github.com>
2024-03-21 10:16:00 +0000

    variables

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,14 @@
+const accountId = 144553;
+let accountEmail = "abc@xyz.com";
+var accountPassword = "12345";
+accountCity = "Jaipur";
+
+// accountId = 2; // not allowed
+accountEmail = "hc@hc.com";
+accountPassword = '323232';
+accountCity = "Banglore";
+let accountState;
+
+console.log(accountId);
+
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState]);
