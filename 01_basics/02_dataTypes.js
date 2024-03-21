c1856be88fd73cf9f42fdfb2aeec86e67117271e
Mayank Soni <92715309+mynk30@users.noreply.github.com>
2024-03-21 11:02:48 +0000

    data types

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,28 @@
+"use strict"; // treates all js code as new version
+
+// alert(3+3);
+
+console.log(3+
+    3); // code redability should be higher
+    
+console.log("JavaScript");
+
+let name = "Mayank";
+let age = 18;
+let isLoggedIn = false;
+
+// number 
+// bigInt
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined
+// symbol => unique
+
+
+// object
+
+console.log(typeof(undefined)); // undefined
+console.log(typeof(null)); // object
+
+
