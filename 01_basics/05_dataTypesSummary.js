3ace3e394001d45716a5520396e7a67859f647bb
Mayank <mayanksoni920@gmail.com>
2024-03-22 20:33:55 +0530

    stack and heap

--- 01_basics/05_dataTypesSummary.js
+++ 01_basics/05_dataTypesSummary.js
@@ -31,5 +31,31 @@ const myFunction = function(){
     console.log("Hello World");
 };
 
-console.log(typeof anotherId);
+// console.log(typeof anotherId);
 // https://262.ecma-international.org/5.1/#sec-11.4.3
+
+
+// ++++++++++++++++ Stack and Heap+++++++++++++++++++++ 
+// Stack (Primitive), Heap (Non-primitive)
+
+let fullName = "mayankSoni";
+let anotherFullName = fullName;
+console.log(fullName);
+console.log(anotherFullName);
+
+anotherFullName = "chaiaurcode";
+console.log(fullName);
+console.log(anotherFullName);
+
+let userOne = {
+    email: 'user@google.com',
+    upi: "user@ybl",
+}
+
+let userTwo = userOne
+console.log(userOne)
+console.log(userTwo)
+
+userTwo.email = "mayank@google.com";
+console.log(userOne.email)
+console.log(userTwo.email)
