909723710c005feb577bb78e717ae2770b5dd2fe
Mayank <mayanksoni920@gmail.com>
2024-03-24 23:31:51 +0530

    mini hoisting

--- 03_basics/02_scopes.js
+++ 03_basics/02_scopes.js
@@ -3,14 +3,50 @@ let a = 300;
 if (true) {
     let a = 10;
     const b = 20;
-    console.log("INNER: ", a);
+    // console.log("INNER: ", a);
 }
 
-for (let i = 0; i < array.length; i++) {
-    const element = array[i];
-    
-}
-
-console.log(a);
+// console.log(a);
 // console.log(b);
 // console.log(c);
+
+// Nested Scope
+function one() {
+    const username = "mayank";
+
+    function two(){
+        const website = "youtube.com";
+        console.log(username);
+    }
+    // console.log(website);
+
+    // two();
+}
+
+// one();
+
+if(true) {
+    const username = "mayank";
+    if(username === "mayank") {
+        const website = " youtube.com";
+        console.log(username + website);
+    }
+    // console.log(website);
+}
+
+// console.log(username);
+
+// +++++++++++++++ interesting ++++++++++++++++++++
+console.log(addOne(5));
+
+function addOne(num) {
+    return num+1;
+}
+
+
+
+addTwo(5);
+
+const addTwo = function (num) {
+    return num + 2;
+}
