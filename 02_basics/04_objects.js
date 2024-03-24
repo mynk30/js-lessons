26c3cc7072e241d0281d5415e59811536adadd53
Mayank <mayanksoni920@gmail.com>
2024-03-24 13:42:41 +0530

    json

--- 02_basics/04_objects.js
+++ 02_basics/04_objects.js
@@ -34,7 +34,7 @@ const obj2 = {
 // const obj3 = Object.assign(obj1, obj2)
 // const obj3 = Object.assign({}, obj1, obj2)
 const obj3 = {...obj1, ...obj2}; // spread
-console.log(obj3);
+// console.log(obj3);
 
 
 const users = [
@@ -48,12 +48,42 @@ const users = [
     }
 ]
 
-console.log(users[1].email);
-console.log(tinderUser);
+// console.log(users[1].email);
+// console.log(tinderUser);
 
-console.log(Object.keys(tinderUser));
-console.log(Object.values(tinderUser));
-console.log(Object.entries(tinderUser));
+// console.log(Object.keys(tinderUser));
+// console.log(Object.values(tinderUser));
+// console.log(Object.entries(tinderUser));
 
 
-console.log(tinderUser.hasOwnProperty('isLogged'));
+// console.log(tinderUser.hasOwnProperty('isLogged'));
+
+// Object destructuring and JSON API
+
+const course = {
+    coursename: "js in hindi",
+    price: "999",
+    courseInstructor: "hitesh"
+}
+
+// course.courseInstructor
+
+const {courseInstructor: instructor} = course
+
+// console.log(courseInstructor);
+// console.log(instructor);
+
+// JSON 
+
+// {
+//     "name": "hitesh",
+//     "courseName": "js in hindi",
+//     "price": "free"
+// }
+
+// Array of json
+[
+    {},
+    {},
+    {}
+]
