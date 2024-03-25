dc93424b24597dbda19a5dfb51205ba9993acfb7
Mayank <mayanksoni920@gmail.com>
2024-03-25 14:45:52 +0530

    switch

---
+++ 04_controlFlow/2_switch.js
@@ -0,0 +1,33 @@
+// switch (key) {
+//     case value:
+
+//         break;
+
+//     default:
+//         break;
+// }
+
+const month = "feb";
+switch (month) {
+    case "jan":
+        console.log("January");
+        break;
+    case "feb":
+        console.log("February");
+        break;
+    case "mar":
+        console.log("March");
+        break;
+    case "apr":
+        console.log("April");
+        break;
+    case "may":
+        console.log("May");
+        break;
+    case "jun":
+        console.log("June");
+        break;
+    default:
+        console.log("Invalid Month");
+        break;
+}
