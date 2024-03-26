1729d5ba30e90262844748ba0dc1ad0248be1e6e
Mayank <mayanksoni920@gmail.com>
2024-03-26 17:02:40 +0530

    reduce

---
+++ 05_iterations/8_eight.js
@@ -0,0 +1,34 @@
+// reduce - used to sum up values inside an array
+const myNums = [1, 2, 3];
+
+// const myTotal = myNums.reduce((acc, currVal)=>{
+//     console.log(`acc: ${acc} and currVal: ${currVal}`);
+//     return acc + currVal;
+// }, 0)
+
+const myTotal = myNums.reduce((acc, currVal) => acc + currVal, 0)
+
+console.log(myTotal);
+
+const shoppingCart = [
+    {
+        itemName: 'js course',
+        price: 2999
+    },
+    {
+        itemName: 'py course',
+        price: 999
+    },
+    {
+        itemName: 'mobile dev',
+        price: 5999
+    },
+    {
+        itemName: 'data scientist course',
+        price: 12999
+    },
+];
+
+const priceToPay = shoppingCart.reduce((acc, item) => acc + item.price, 0);
+
+console.log(priceToPay);
