5139f614e105f1f24d2f37ceb77e57492b1f8b15
Mayank <mayanksoni920@gmail.com>
2024-04-10 17:15:12 +0530

    oops

--- 10_classes_and_OOPS/oop.js
+++ 10_classes_and_OOPS/oop.js
@@ -9,5 +9,25 @@ const user = {
     }
 }
 
-console.log(user.username);
-console.log(user.getUserDetails());
+// console.log(user.username);
+// console.log(user.getUserDetails());
+// console.log(this);
+
+// const promiseOne = new Promise();
+// const date = new Date();
+
+function User(username, loginCount, isLoggedIn){
+    this.username = username;
+    this.loginCount = loginCount;
+    this.isLoggedIn = isLoggedIn;
+
+    return this;
+}
+
+const userOne = new User("mayank", 12, true);
+const userTwo = new User("ChaiAurCode", 11, false);
+
+console.log(userOne);
+console.log(userTwo);
+console.log(userOne.constructor);
+console.log(userTwo.constructor);
