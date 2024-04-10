a8959b79b628838613bf91db86288724855cd5d2
Mayank <mayanksoni920@gmail.com>
2024-04-10 12:37:57 +0530

    classes and oops

---
+++ 10_classes_and_OOPS/oop.js
@@ -0,0 +1,13 @@
+const user = {
+    username: "mayank",
+    loginCount: 8,
+    signedIn: true,
+    getUserDetails: function(){
+        // console.log("Got user details from database");
+        // console.log(`Username: ${this.username}`);
+        console.log(this);
+    }
+}
+
+console.log(user.username);
+console.log(user.getUserDetails());
